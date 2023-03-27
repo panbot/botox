4735d35a759bfafa48a0e9ef74044bd398efeec0
Botao Pan <botao_pan@gtfund.com>
2023-03-27 22:07:19 +0800

    fix expandify return type

--- src/lib/expandify.ts
+++ src/lib/expandify.ts
@@ -1,14 +1,16 @@
 function expandify<O extends {}>(o: O) {
 
-    const expander: {
-        <Expansion>(by: (base: O) => Expansion): O & Expansion,
-        <Expansion>(by: Expansion & ThisType<O & Expansion>): O & Expansion,
-    } = (
-        by: any
-    ) => Object.assign(o, typeof by == 'function' ? by(o) : by);
-
     return Object.assign(o, {
-        [expandify.expand]: expander,
+        [expandify.expand]: <EXPANSION extends {}>(
+            by: ( (base: O) => EXPANSION ) | EXPANSION & ThisType<O & EXPANSION>
+        ) => expandify(
+            Object.assign(
+                o,
+                typeof by == 'function'
+                    ? (by as (base: O) => EXPANSION)(o)
+                    : by as EXPANSION
+            )
+        ),
     })
 }
 
