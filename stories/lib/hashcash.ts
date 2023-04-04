777172a2396b5485ba97d20e35896c92d732e91b
Botao Pan <botao_pan@gtfund.com>
2023-04-04 14:05:20 +0800

    stories for hashcash & logging

---
+++ stories/lib/hashcash.ts
@@ -0,0 +1,42 @@
+import factory from '@/lib/hashcash';
+
+const hashcash = factory('sha256');
+
+@hashcash.install(5)
+class MyProtectedApi {
+
+    @hashcash.install(5)
+    some_method() {
+
+    }
+}
+
+
+let request = {
+    body: JSON.stringify(({api: 'MyProtectedApi', params: {}})),
+    headers: {
+        get 'x-hashcash'() {
+            while (true) {
+                let cash = Math.random().toString();
+                let zeros = hashcash.consume_hashcash(request.body, cash);
+                if (zeros >= 4) return cash;
+            }
+        },
+    }
+};
+
+hashcash.set_provided_difficulty(
+    request,
+    hashcash.consume_hashcash(request.body, request.headers['x-hashcash']),
+);
+
+let api = new MyProtectedApi();
+if (hashcash.check(request, api)) {
+    console.log('pass');
+} else {
+    console.log(
+        'fail',
+        'required difficulty', hashcash.get_resource_difficulty(api),
+        'provided difficulty', hashcash.get_provided_difficulty(request),
+    );
+}
---
+++ stories/lib/logging/levels.ts
@@ -0,0 +1,57 @@
+import di from '@/lib/dependency-injection';
+import proxitive_aop from '@/lib/aop/proxitive';
+import logging from '@/lib/logging';
+import { assert_true } from 'stories/asserts';
+import { IS } from '@/lib/types';
+
+const levels = {
+    debug: 0,
+    info: 1,
+    warn: 2,
+    crit: 3,
+    log: 4,
+};
+
+const level = 2;
+
+{
+    const log = (...args: any) => {};
+
+    const loggers = logging.create_loggers(log, levels, level);
+
+    type LOGGERS = typeof loggers;
+
+    assert_true<IS< LOGGERS, { [ P in keyof typeof levels]: typeof log } >>();
+}
+
+{
+    const container = di();
+    const aop = proxitive_aop(proxify => container.on('instantiated', proxify));
+
+    const decorators = logging.create_decorators(
+        aop.after,
+        levels,
+        level,
+    );
+
+
+    class MyService {
+
+        @decorators.debug((e, r) => {
+
+        })
+        async someAsyncMethod(...args: number[]) {
+            if (args.length > 5) throw new Error(`too many args`);
+            return args.reduce((pv, cv) => pv + cv);
+        }
+
+        @decorators.debug((e, r) => {
+
+        })
+        someMethod(...args: number[]) {
+            if (args.length > 5) throw new Error(`too many args`);
+            return args.reduce((pv, cv) => pv + cv);
+        }
+    }
+
+}
